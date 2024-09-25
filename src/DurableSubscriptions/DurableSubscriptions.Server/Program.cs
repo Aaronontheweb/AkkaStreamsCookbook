71468519f84273897f96b5e9ee834ee3c643150c
Aaron Stannard <aaron@petabridge.com>
2024-09-25 14:12:15 -0500

    fixed app startup

--- src/DurableSubscriptions/DurableSubscriptions.Server/Program.cs
+++ src/DurableSubscriptions/DurableSubscriptions.Server/Program.cs
@@ -2,21 +2,49 @@
 using Akka.Cluster.Hosting;
 using Akka.Cluster.Sharding;
 using Akka.Hosting;
+using Akka.Persistence.Sql.Config;
+using Akka.Persistence.Sql.Hosting;
 using Akka.Remote.Hosting;
 using DurableSubscriptions.Server.Actors;
 using DurableSubscriptions.Shared;
+using LinqToDB;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
 
 var hostBuilder = new HostBuilder();
 
+hostBuilder.ConfigureAppConfiguration((context, builder) =>
+{
+    builder
+        .AddJsonFile("appsettings.json")
+        .AddJsonFile($"appsettings.{environment}.json", true)
+        .AddEnvironmentVariables();
+});
+
+hostBuilder.ConfigureLogging(builder =>
+{
+    builder.AddConsole();
+});
+
 hostBuilder.ConfigureServices((context, services) =>
 {
+    var connectionString = context.Configuration.GetConnectionString("DefaultConnection");
+    
     services.AddAkka("DurableSubs", (builder, sp) =>
     {
-        builder.ConfigureLoggers(loggers => { loggers.ClearLoggers(); })
+        builder.ConfigureLoggers(c =>
+            {
+                c.ClearLoggers();
+                c.AddLoggerFactory();
+                c.LogLevel = Akka.Event.LogLevel.InfoLevel;
+            })
             .WithRemoting(new RemoteOptions { Port = 9914, HostName = "localhost" })
             .WithClustering(new ClusterOptions()
                 { SeedNodes = ["akka.tcp://DurableSubs@localhost:9914"], Roles = ["subscriptions"] })
+            .WithSqlPersistence(connectionString, ProviderName.PostgreSQL, tagStorageMode:TagMode.TagTable)
             .WithShardRegion<ProductInventoryActor>("products",
                 s => Props.Create(() => new ProductInventoryActor(new ProductId(s))),
                 HashCodeMessageExtractor.Create(50, EntityIdExtractor), new ShardOptions()
--- src/DurableSubscriptions/README.md
+++ src/DurableSubscriptions/README.md
@@ -1,1 +1,18 @@
 # DurableSubscriptions          
+
+## Prerequisites
+
+- Docker installed on your machine.
+- Ensure that the `start_postgres.sh` script has been run to initialize the PostgreSQL container with the necessary database before starting the application.
+
+## Getting Started
+
+### Step 1: Run the PostgreSQL Docker Container
+
+Before launching the application, you need to set up the PostgreSQL database that the application depends on. To do this, run the following command to execute the `start_postgres.sh` script:
+
+```bash
+./start_postgres.sh
+```
+
+This will launch a Postgres instance on host port `5433`.
